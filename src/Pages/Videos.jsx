286dd33aa0c321a6fd4f3744e4052cb5e14d57ec
seungmin-lee-dotcom <k07217@naver.com>
2023-01-13 16:34:06 +0900

    axios refactoring

--- src/Pages/Videos.jsx
+++ src/Pages/Videos.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import VideoCard from "../components/VideoCard";
+import axios from "axios";
 
 export default function Videos() {
   const { keyword } = useParams();
@@ -10,9 +11,9 @@ export default function Videos() {
     error,
     data: videos,
   } = useQuery(["videos", keyword], async () => {
-    return fetch(`/videos/${keyword ? "search" : "popular"}.json`)
-      .then((res) => res.json())
-      .then((data) => data.items);
+    return axios
+      .get(`/videos/${keyword ? "search" : "popular"}.json`)
+      .then((res) => res.data.items);
   });
   return (
     <>
