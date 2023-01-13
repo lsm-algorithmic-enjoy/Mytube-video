1dfaa638fb6780ba044271a5b7e73816b90ed16e
seungmin-lee-dotcom <k07217@naver.com>
2023-01-13 16:26:48 +0900

    reactquery video

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { Outlet } from "react-router-dom";
 import SearchHeader from "./components/SearchHeader";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
       <SearchHeader />
-      <Outlet />
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+      </QueryClientProvider>
     </>
   );
 }
--- src/Pages/Videos.jsx
+++ src/Pages/Videos.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import VideoCard from "../components/VideoCard";
 
 export default function Videos() {
   const { keyword } = useParams();
-  return <div>Videos {keyword ? keyword : "🔥Hot video"}</div>;
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery(["videos", keyword], async () => {
+    return fetch(`/videos/${keyword ? "search" : "popular"}.json`)
+      .then((res) => res.json())
+      .then((data) => data.items);
+  });
+  return (
+    <>
+      <div>Videos {keyword ? keyword : "So Hot"}</div>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Something is wrong</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
