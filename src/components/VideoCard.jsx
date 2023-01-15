c253893852ddd3918cf6cbad43830ebc66fa4515
seungmin-lee-dotcom <k07217@naver.com>
2023-01-15 16:47:01 +0900

    videocard styling

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -5,11 +5,11 @@ export default function VideoCard({ video }) {
   const { title, thumbnails, channelTitle, publishedAt } = video.snippet;
   return (
     <li>
-      <img src={thumbnails.medium.url} alt={title} />
+      <img className="w-full" src={thumbnails.medium.url} alt={title} />
       <div>
-        <p>{title}</p>
-        <p>{channelTitle}</p>
-        <p>{formatAgo(publishedAt, "ko")}</p>
+        <p className="font-semibold my-2 line-clamp-2">{title}</p>
+        <p className="text-sm opacity-80">{channelTitle}</p>
+        <p className="text-sm opacity-60">{formatAgo(publishedAt, "ko")}</p>
       </div>
     </li>
   );
