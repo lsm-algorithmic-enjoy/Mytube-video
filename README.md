a2bd9b24f5b38523f349ad45757b43186e77bad8
seungmin-lee-dotcom <k07217@naver.com>
2023-01-15 12:24:18 +0900

    reactquery contextapi

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ About Project
 1. react-router-dom을 이용한 페이지 라우팅 기능
 2. Outlet을 이용한 SearchHeader 구현
 3. Tailwind-CSS를 이용한 스타일링
+4. React-Query 및 Context Api 적용
 
 Netlify deployment: https://mytubesite.netlify.app/
--- src/Pages/Videos.jsx
+++ src/Pages/Videos.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import VideoCard from "../components/VideoCard";
-import axios from "axios";
+
+import Youtube from "../api/Youtube";
+import FakeYoutube from "../api/fakeYoutube";
+import { useYoutubeApi } from "../context/YoutubeApiContext";
 
 export default function Videos() {
   const { keyword } = useParams();
+  const { youtube } = useYoutubeApi();
   const {
     isLoading,
     error,
     data: videos,
-  } = useQuery(["videos", keyword], async () => {
-    return axios
-      .get(`/videos/${keyword ? "search" : "popular"}.json`)
-      .then((res) => res.data.items);
-  });
+  } = useQuery(["videos", keyword], () => youtube.search(keyword));
   return (
     <>
       <div>Videos {keyword ? keyword : "So Hot"}</div>
